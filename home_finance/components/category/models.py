e527a3bbd8f0c996b9fc628896dd175ad3bdf71f
Tom Goetze <tgoetze@localhost.localdomain>
2020-08-23 07:08:43 -0700

    Remove transactions from Category table.
    Change to have Transactions link to the Category table and allow it to be null.
    A null category field in the Transaction table means it is the single parent transaction representing the entire summary transaction.
    In this case, there will be child transactions that show all the line items that make up the transaction.

--- home_finance/components/category/models.py
+++ home_finance/components/category/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from home_finance.components.transaction.models import Transaction
-
 
 class Category(models.Model):
     """
@@ -10,4 +8,3 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, blank=True, null=True)
     parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='subcategories')
-    transactions = models.ManyToManyField(Transaction, blank=True, null=True)
--- home_finance/components/transaction/models.py
+++ home_finance/components/transaction/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from home_finance.components.external_account.models import ExternalAccount
+import home_finance.components.category.models as category
+import home_finance.components.external_account.models as external_account
 
 
 class Transaction(models.Model):
@@ -7,7 +8,7 @@ class Transaction(models.Model):
     Model definition for a Transaction.
     """
 
-    account = models.ForeignKey(ExternalAccount, on_delete=models.CASCADE)
+    account = models.ForeignKey(external_account.ExternalAccount, on_delete=models.DO_NOTHING)
     parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='subtransactions')
     description = models.CharField(max_length=200)
     amount = models.FloatField()
@@ -15,3 +16,7 @@ class Transaction(models.Model):
     notes = models.CharField(max_length=200, blank=True, null=True)
     transfer = models.BooleanField(default=False)
     reconciled = models.BooleanField(default=False)
+    ''' A transaction with no category means it should have children transactions--so it represents a split.
+        In this case, the amount is the summary amount of the splits, so it represents the entire transaction amount.
+    '''
+    category = models.ForeignKey(category.Category, null=True, on_delete=models.DO_NOTHING)
